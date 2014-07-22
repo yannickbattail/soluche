89a3446a176b5e6be859984097a986c95ea4673b
Yannick Battail <yannick.battail@gmail.com>
2014-07-22 16:33:38 +0200

    test tooltip

--- pages/layoutBottom.php
+++ pages/layoutBottom.php
@@ -1,4 +1,9 @@
 
 
+<script>
+
+$('.congress').tooltip({ content: '<table class="inventory"><tr class="odd"><td><img src="images/objets/poule.png" class="inventoryImage" title="poule" /></td><td></td></tr><tr class="even"><td>Permanant</td><td>oui</td></tr><tr class="odd"><td>Notoriété</td><td><span style="color: chartreuse;">+1</span></td></tr><tr class="even"><td>Verre</td><td><span style="">+0</span></td></tr><tr class="odd"><td>Verre optimum</td><td><span style="">+0</span></td></tr><tr class="even"><td>Verre max</td><td><span style="">+0</span></td></tr><tr class="odd"><td>Fatigue</td><td><span style="">+0</span></td></tr><tr class="even"><td>Fatigue max</td><td><span style="color: red;">-1</span></td></tr><tr class="odd"><td>Sexe appeal</td><td><span style="color: chartreuse;">+3</span></td></tr></table>' });
+
+</script
 </body>
 </html>
